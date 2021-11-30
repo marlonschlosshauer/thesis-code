cf550f7eeab64b2e2cf912f97052d701016136f7
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-11-30 17:39:41 +0100

    add test config

--- src/test/code/bind_test.cljs
+++ src/test/code/bind_test.cljs
@@ -1,3 +1,7 @@
-(ns code.bind_test
+(ns code.bind-test
   (:require [cljs.test :refer-macros [async deftest is testing]]
-            [code.bind :refer [bind return show runner]]))
+            [reacl-c.dom :as dom]
+            [code.bind :as core]))
+
+(deftest return-creates-prog
+  (is (core/prog? (core/return (dom/h2 "return-creates-prog")))))

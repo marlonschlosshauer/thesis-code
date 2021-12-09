1fffe8d1ceb4ec18df50fc2b133be6b753c9fce6
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-12-09 19:08:07 +0100

    bind :: Prog a -> (a -> Prog b) -> Bind && runner :: Bind -> Item

--- src/main/code/bind.cljs
+++ src/main/code/bind.cljs
@@ -13,25 +13,19 @@
 (defn commit? [x] (instance? Commit x))
 (defn commit-payload [commit] (:payload commit))
 
-(defrecord Bind [item continuation])
+;; Bind :: Prog a -> (a -> Prog b) -> Bind
+(defrecord Bind [prog continuation])
 (defn make-bind
-  [item continuation]
-  (->Bind item continuation))
+  [prog continuation]
+  (->Bind prog continuation))
 
 (defn bind? [x] (instance? Bind x))
 (defn bind-continuation [b] (:continuation b))
-(defn bind-item [b] (:item b))
+(defn bind-item [b] (:prog b))
 
 (defrecord Prog [item])
 (defn prog? [x] (instance? Prog x))
 (defn make-prog [item] (->Prog item))
-
-;; TODO: mondic return && monadic bind
-;; Functionality of both is currently in "runner"
-;; return: a -> Prog a
-;; bind: Prog a -> (a -> Prog b) -> Prog b
-
-;; Prog is an item that emits commit (eventually)
 
 (defn return [item]
   (make-prog item))
@@ -39,42 +33,23 @@
 (defn show [prog]
   (:item prog))
 
-(defn runner [prog]
+;; runner :: Bind -> Item
+(defn runner [b]
   ;; TODO: Assert (item? prog)
   (c/isolate-state
-   prog
+   b
    (c/dynamic
     (fn [st]
       (c/handle-action
-       ;; TODO: Focus on outter state
-       (if (bind? st) (bind-item st) st)
+       (if (bind? st)
+         ;; TODO: Focus on outter state
+         (bind-item (show st))
+         st)
        (fn [st ac]
          (if (and (commit? ac) (bind? st))
            ;; Save resulting bind/element in state
            (c/return :state ((:continuation st) (commit-payload ac))))))))))
 
-(defn _bind [prog f]
-  ;; show prog until commit happens then call f with result
-  ;; TODO: break loop after initial commit
-  (return
-   (c/isolate-state
-    prog
-    (c/dynamic
-     (fn [st]
-       (c/handle-action
-        (show st)
-        (fn [st ac]
-          (if (commit? ac)
-            (c/return :state (f (commit-payload ac)))))))))))
-
-(defn __bind [prog f]
-  ;; show prog until commit happens then call f with result
-  (return
-   (c/handle-action
-    (show prog)
-    (fn [_ ac]
-      (if (commit? ac) (c/return :state (f (commit-payload ac)))))))
-  )
 
 (defn bind [prog f]
   ;; show prog until commit happens then call f with result
@@ -93,4 +68,3 @@
             ;; Scenario: commit already responded to, how to bubble up value?
             ;; How do you know if you are done?
             (c/return :state (assoc st :prog (f (commit-payload ac)) :called true))))))))))
-

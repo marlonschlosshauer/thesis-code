f0b7e8bb4333400b87a519706ed091d5707170e8
marlonschlosshauer <marlonschlosshauer@gmail.com>
2022-01-05 18:41:26 +0100

    fix associativity in then fn (cps-transform)

--- src/main/code/bind.cljs
+++ src/main/code/bind.cljs
@@ -15,18 +15,36 @@
 (defn commit-payload [commit] (:payload commit))
 
 ;; Bind :: Prog a -> (a -> Prog b) -> Bind b
-(defrecord Bind [prog continuation])
+(defrecord Bind [prog continuation called])
+;; TODO: auto wrap item with Prog
 (defn make-bind
   [prog continuation]
-  (->Bind prog continuation))
+  (->Bind prog continuation false))
 
 (defn bind? [x] (instance? Bind x))
 (defn bind-continuation [b] (:continuation b))
 (defn bind-item [b] (:prog b))
+(defn bind-called [b] (:called b))
 
 (defrecord Prog [item])
 (defn prog? [x] (instance? Prog x))
+(defn prog-item [x] (:item x))
 (defn make-prog [item] (->Prog item))
+
+(defn thena [item cont]
+  (make-bind (if (c/item? item) (make-prog item) item) cont))
+
+;; CPS transformation
+;; on bind: check if left hand is already a bind, re-write to make consumption easier!
+(defn then [prog cont]
+  {:pre [(or (bind? prog) (prog? prog) (c/item? prog))]
+   :post [(bind? %)]}
+  (if (bind? prog)
+    (make-bind (bind-item prog) (fn [x] (then ((bind-continuation prog) x) cont)))
+    (make-bind (if (c/item? prog) (make-prog prog) prog) cont)))
+
+(defn bind-or-prog? [x]
+  (or (prog? x) (bind? x)))
 
 (defn return
   "Signify that this Item is a Prog, therefore emitting a commit at some point"
@@ -34,9 +52,12 @@
   (make-prog item))
 
 (defn show
-  "Display Item inside of a Prog"
-  [prog]
-  (:item prog))
+  "Display Item inside of Prog"
+  [x]
+  (cond
+    (prog? x) (prog-item x)
+    (bind? x) (prog-item (bind-item x))
+    (c/item? x) x))
 
 ;; runner :: Bind -> Item
 (defn runner
@@ -49,13 +70,13 @@
     (fn [st]
       (c/handle-action
        (cond
-         (bind? st) (show (bind-item st)) ;; TODO: Flatmap?
+         (bind? st) (show (bind-item st))
          (prog? st) (show st) ;; todo
-         (c/item? st) st)
+         :else st)
        (fn [st ac]
          (if (and (commit? ac) (bind? st))
            ;; Save resulting bind/element in state
            ;; TODO: Fix f call at end
            ;; Scenario: commit already responded to, how to bubble up value?
            ;; How do you know if you are done?
-           (c/return :state ((:continuation st) (commit-payload ac))))))))))
+           (c/return :state ((bind-continuation st) (commit-payload ac))))))))))

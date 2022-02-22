1d6de406706debe0240a4d916846c155f56163dd
marlonschlosshauer <marlonschlosshauer@gmail.com>
2022-02-22 19:20:39 +0100

    cleanup macros

--- src/main/code/macros.clj
+++ src/main/code/macros.clj
@@ -3,13 +3,8 @@
 (defmacro then
   "Bind a `Prog` to a continuation. Returns a `Bind`. cont should return a `Bind` or `Prog`"
   [[var val & rest :as steps] end-expr]
-  ;; {:pre [(even? (count steps))
-  ;;        (or (code.bind/bind? val)
-  ;;            (code.bind/prog? val))
-  ;;        (fn? end-expr)]}
   (if steps
     `(code.bind/then ~val (fn [~var] (then ~rest ~(seq end-expr))))
-    ;; TODO: end-expr has arity 0, could check for last var and pass
     `(~end-expr)))
 
 (defmacro runner

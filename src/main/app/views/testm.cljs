887698ca16e6f7acb881a64d08cf1f2133167499
marlonschlosshauer <marlonschlosshauer@gmail.com>
2022-03-18 10:04:38 +0100

    twice-bug (bind.cljs:190, testm.cljs:63)

--- src/main/app/views/testm.cljs
+++ src/main/app/views/testm.cljs
@@ -60,77 +60,23 @@
             ] (fn [])) ;; error removed to allow for compilation
    ))
 
-(comment
-  (def test-end-expr-return-state
-  (c/local-state
-   {:number 0}
-   (c/handle-action
-    (dom/div
-     (c/with-state-as st
-       (dom/pre (pr-str st)))
-     (c/fragment
-      (c/init
-       (c/return :state "haha"))))
-    (fn [_ ac]
-      (c/return :state ac))))))
-
-(comment
-  (def main
-    (dom/div
-     (dom/div
-      (dom/h2 "mix primitives and macros")
-      mix-primitives-macros)
-     (dom/div
-      (dom/h2 "implicit state leak through runner")
-      implicit-state-leak-through-runner)
-     (dom/div
-      (dom/h2 "stress but with macros")
-      (stress-with-macro 0))
-     (dom/div
-      (dom/h2 "emit through runner")
-      emit-through)
-     (dom/div
-      (dom/h2 "test uneven count then macro")
-      test-uneven-count-then)
-     (dom/div
-      (dom/h2 "test end expr return state")
-      test-end-expr-return-state))))
-
-(def woof
-  (u/wrap-state
-   (c/local-state
-    {:product 534 :status :purchased}
-    (u/wrap-state
-     (c/dynamic
-      (fn [[o i]]
-        (dom/div
-         (dom/h2 "woof")
-         (c/fragment
-          (c/init
-           (c/return :state [(assoc o :version 5) {:product 534 :status :payed}])))))
-      )))))
-
 (def test-end-expr-return-state
   (c/local-state
    1
    (c/dynamic
      (fn [[bla state]]
        (dom/div
-        (dom/pre (pr-str [bla state]))
-        (m/runner [_ (item "123")
-                   second (item 5)]
+        ;;(dom/pre (pr-str [bla state]))
+        (m/runner [a (item 1)
+                   b (item 2)]
                   (fn [[o i]]
-                    (c/return
-                     :state
-                     [[(assoc o :version second) i] inner]))))))))
-
- ;; TODO: kriegt fn letzten Wert?
-
+                    (println (str "test: " (pr-str [o i])))
+                    (c/return :state [(assoc o :version b) i])
+                    )))))))
 
 (def main
   (dom/div
      (dom/h2 "test end expr return state")
-     test-end-expr-return-state)
-  ;;woof
-  )
+     test-end-expr-return-state))
+
 
--- src/main/code/bind.cljs
+++ src/main/code/bind.cljs
@@ -69,36 +69,218 @@
   ([[_ & rest] small]
    (vec (cons small rest))))
 
-(defn runner
-  "Show `Prog` (or `Bind`). Returns an `Item`"
-  [b]
-  {:pre [(or (bind? b) (prog? b))]}
-  ;; inner state of book-keeping is the bind
+(comment
+  (defn runner
+  [prog]
   (c/local-state
-   b
+   prog
+   (c/dynamic
+    (fn [[outter inner]]
+      (cond
+        (nil? inner)
+        (c/fragment)
+        (fn? inner)
+        (c/focus
+         first-lens
+         (c/fragment
+          (c/once (fn [] (inner [outter])))
+          (c/finalize (fn [] (c/return :state [[outter nil]])))))
+        :else
+        (c/handle-action
+         (c/focus
+          first-lens
+          (show inner))
+         (fn [[outter st] ac]
+           (if (and (commit? ac) (bind? st))
+             (c/return :state [outter ((bind-continuation st) (commit-payload ac))])
+             (c/return :action ac))))))))))
+
+
+(comment
+  (defn runner
+  [prog]
+  (c/local-state
+   prog
    (c/dynamic
     (fn [[outter inner]]
       (dom/div
-       (dom/samp (pr-str inner))
-       (cond
-         (nil? inner) (c/fragment)
-         (fn? inner) (c/fragment
-                      (c/focus
-                       first-lens
-                       (dom/div
-                        (dom/samp (pr-str inner))
-                        (c/once (fn [] (inner outter))))))
-         :else (c/handle-action
-                ;; display item in bind/prog
-                (c/focus
-                 first-lens
-                 (show inner))
-                (fn [[outter st] ac]
-                  ;; call continuation of bind on commit
-                  (if (and (commit? ac) (bind? st))
-                    (c/return :state [outter ((bind-continuation st) (commit-payload ac))])
-                    (c/return :action ac))))
-         ))))))
+       (dom/samp (pr-str [outter inner]))
+       (if (nil? inner)
+         (c/fragment)
+         (c/handle-action
+          (c/focus
+           first-lens
+           (show inner))
+          (fn [[outter st] ac]
+            ;; Trying "done" type
+            (if (and (commit? ac) (bind? st))
+              (let [res ((bind-continuation st) (commit-payload ac))]
+                (c/return :state (if (bind? res) ;; for some reason can't check with map?
+                                   [outter res]
+                                   [((:end-expr res) outter) nil]
+                                   ;;[outter nil] ;; doesn't run twice
+                                   ;;[nil nil] ;;  does run twice
+                                   )))
+              (c/return :action ac)))))))))))
 
 
 
+
+;; state change of outter causes re-draw of runner, which then starts at step 0 again
+
+(comment
+  (defn runner
+    [prog]
+    (c/local-state
+     prog
+     (c/dynamic
+      (fn [[outter inner]]
+        (println (pr-str inner))
+        (if (nil? inner)
+          (c/fragment)
+          (c/handle-action
+           (c/focus
+            first-lens
+            (show inner))
+           (fn [[outter st] ac]
+             ;; Trying "done" type
+             (if (and (commit? ac) (bind? st))
+               (let [res ((bind-continuation st) (commit-payload ac))]
+                 (c/return :state (if (bind? res) ;; for some reason can't check with map?
+                                    [outter res]
+                                    ;;[((:end-expr res) outter) nil]
+                                    [outter nil] ;; doesn't run twice
+                                    ;;[nil nil] ;;  does run twice
+                                    )))
+               (c/return :action ac))))))))))
+
+
+(comment
+  (c/return :state
+            (if (and
+                 (fn? (bind-continuation st))
+                 (map? ((bind-continuation st) [nil nil])))
+              [((:end-expr (bind-continuation st)) (commit-payload ac)) nil]
+              [outter ((bind-continuation st) (commit-payload ac))])))
+
+
+(comment
+  (defn runner
+  [prog]
+  (c/local-state
+   prog
+   (c/dynamic
+    (fn [[outter inner]]
+      (if (nil? inner)
+        (c/fragment)
+        (c/handle-action
+         (c/focus
+          first-lens
+          (show inner))
+         (fn [[outter st] ac]
+           (if (commit? ac)
+             (cond
+               (bind? st) (c/return :state [outter ((bind-continuation st) (commit-payload ac))])
+               (fn? st) (st outter)
+               :else (c/return :action ac))
+             (c/return :action ac)))))))))
+
+)
+
+
+(defn runner
+    [prog]
+    (c/local-state
+     prog
+     (c/dynamic
+      (fn [[outter inner]]
+        (println (pr-str inner))
+        (dom/div
+         (c/fragment
+          ;; (c/once (fn []
+          ;;           (println "moin!")
+          ;;           (c/return)))
+          (c/cleanup (fn []
+                       (println "goodbye!")
+                       (c/return))))
+         (dom/samp (pr-str [outter inner]))
+         (if (nil? inner)
+           (c/fragment)
+           (c/handle-action
+            (c/focus
+             first-lens
+             (show inner))
+            (fn [[outter st] ac]
+              ;; Trying "done" type
+              (if (and (commit? ac) (bind? st))
+                (let [res ((bind-continuation st) (commit-payload ac))]
+                  (c/return :state (if (bind? res) ;; for some reason can't check with map?
+                                     [outter res]
+                                     ;;[((:end-expr res) outter) nil]
+                                     ;;[outter nil] ;; doesn't run twice
+                                     [nil nil] ;;  does run twice
+                                     )))
+                (c/return :action ac))))))))))
+
+
+;;
+
+
+
+
+;; The problem is that inner needs to be set to nil after end-expr is executed (which probably is :return)
+;; Obvious solution would be to pass end-expr to the runner. This however does not work with the current macros. end-expr might want to access results of prev. steps. To have access to them it needs to come from a then. Otherwise it's not path of the nested then calls.
+
+;; Possible impl. if end-expr argument:
+(comment
+  (defn runner
+    [prog end-expr]
+    (c/local-state
+     prog
+     (c/dynamic
+      (fn [[outter inner]]
+        (if (nil? inner)
+          (end-expr outter)
+          (c/handle-action
+           (c/focus
+            first-lens
+            (show inner))
+           (fn [[outter st] ac]
+             (if (commit? ac)
+               (c/return
+                :state
+                [outter
+                 (if (bind? st)
+                   ((bind-continuation st)
+                    (commit-payload ac))
+                   nil)])
+               (c/return :action ac))))))))))
+
+;; Cannot check if inner == value: What if intermediate is just result
+
+
+
+
+(comment
+;; GOOD WORKING BASE!
+  (defn runner
+  [prog]
+  (c/local-state
+   prog
+   (c/dynamic
+    (fn [[outter inner]]
+      (dom/div
+       (dom/samp (pr-str [outter inner]))
+       (if (nil? inner)
+        (c/fragment)
+        (c/handle-action
+         (c/focus
+          first-lens
+          (show inner))
+         (fn [[outter st] ac]
+           (if (commit? ac)
+             (cond
+               (bind? st) (c/return :state [outter ((bind-continuation st) (commit-payload ac))])
+               (fn? st) (st outter)
+               :else (c/return :action ac))
+             (c/return :action ac)))))))))))
